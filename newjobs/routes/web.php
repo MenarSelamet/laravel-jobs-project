a9887227b8aa15f3a5dd874c47ccd6184e881d88
Menar Selamet <menarselamet007@gmail.com>
2024-08-17 00:37:14 +0300

    Adding relationship functions

--- newjobs/routes/web.php
+++ newjobs/routes/web.php
@@ -8,19 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function ()  {
-    return view('jobs', ['jobs'=> job::all()]);
+    return view('jobs', ['jobs'=> job::all()]);   
 });
 
 Route::get('/jobs/{id}', function ($id)  {
    $job = job::find($id);
     return view('job', ['job'=> $job]);
 });
-         
-      
-
-
-
-
 
 Route::get('/contact', function () {
     return view('contact');
