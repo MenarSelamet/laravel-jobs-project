ce1dd86c63531ac378e18ce8ce9570afa9ba9d60
Menar Selamet <menarselamet007@gmail.com>
2024-08-16 16:21:47 +0300

    Moving dummy data to mysql database and using Eloquent to retrieve the data

--- newjobs/app/Models/Job.php
+++ newjobs/app/Models/Job.php
@@ -1,35 +1,11 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 use \Illuminate\Support\Arr;
 
-class Job { 
-    
-    public static function all():array { return [[ "id"=> 1,
-    'title'=> 'Director',
-    'salary'=> '$50,000'
-    ],
-    [
-    "id"=> 2,
-    'title'=> 'Programmer',
-    'salary'=> '$10,000'
-    ],
-    [
-    "id"=> 3,
-    'title'=> 'Teacher',
-    'salary'=> '$40,000'
-    ]
-    ];
-    } 
+class Job extends Model {
 
-
-public static function find (int $id): array {
-  $jobs = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-  if (! $jobs) {
-    abort(404);
-}
-return $jobs;
-
-
-}}
+protected $table= "job_listings";
+  
+ }
