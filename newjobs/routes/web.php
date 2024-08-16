390ab80251be2e93959673b362e0562e184eb18f
Menar Selamet <menarselamet007@gmail.com>
2024-08-16 12:26:18 +0300

    Turning the job class into a Model

--- newjobs/routes/web.php
+++ newjobs/routes/web.php
@@ -1,27 +1,8 @@
 <?php
 use \Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
-class job {
-    public static function all():array {
-        return [[
-                "id"=> 1,
-            'title'=> 'Director',
-            'salary'=> '$50,000'
-        ],
-         [
-             "id"=> 2,
-            'title'=> 'Programmer',
-            'salary'=> '$10,000'
-         ],
-          [
-             "id"=> 3,
-            'title'=> 'Teacher',
-            'salary'=> '$40,000'
-        ]
-          ];
-} };
-            
 
 Route::get('/', function () {
     return view('home');
