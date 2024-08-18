8887b01e163542d6d69f1130b54e8d2f4f8a5e9b
Menar Selamet <menarselamet007@gmail.com>
2024-08-18 22:45:18 +0300

    Adding details to registration router

--- newjobs/app/Http/Controllers/RegisteredUserController.php
+++ newjobs/app/Http/Controllers/RegisteredUserController.php
@@ -2,32 +2,26 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
-
-use Illuminate\Http\Request;
-
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
 class RegisteredUserController extends Controller
 {
     public function create(){
         return view("auth.register");
     }
      public function store(){
-        request()->validate([
-        'first_name' => ['required', 'min:3'],
-        'last_name' => ['required', 'min:3'],
-        'email' => ['required'],
-        'password'=> ['required'],
-        'password_confirmation'=> ['required'],
-        
+      $validatedAttributes =  request()->validate([
+        'first_name' => ['required'],
+        'last_name' => ['required'],
+        'email' => ['required', 'email'],
+        'password'=> ['required', Password::min(6), 'confirmed'],
+        'password_confirmation'=>['required', Password::min(6)],
     ]);
 
-    User::create([
-        'first_name' => request('first_name'),
-        'last_name' => request('last_name'),
-       'email' => request('email'),
-        'password'=> request('password'),
-        'password_confirmation'=> request('password_confirmation'),
-    ]);
+    $user = User::create($validatedAttributes);
 
-    return redirect('/');
+    Auth::login($user);
+
+    return redirect('/jobs');
     }
 }
