ffaee12f31bc026095a31aa9943d76aa856c708c
Menar Selamet <menarselamet007@gmail.com>
2024-08-18 14:30:07 +0300

    few touches in the routes

--- newjobs/routes/web.php
+++ newjobs/routes/web.php
@@ -2,34 +2,14 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-// Index
+Route::get('/', fn()=>view('home'));
+Route::get('/contact', fn()=>view('contact'));
 Route::get('/jobs', [JobController::class, 'index']); 
-
-// Create
 Route::get('/jobs/create', [JobController::class, 'create']);
-
-// Store
 Route::post('/jobs', [JobController::class,'store']);
-
-// Show
 Route::get('/jobs/{job}', [JobController::class, 'show']);
-
-// Edit
 Route::get('/jobs/{job}/edit', [JobController::class,'edit']);
-
-// Update
 Route::patch('/jobs/{job}',[JobController::class,'update']);
-
-// Destroy
 Route::delete('/jobs/{job}', [JobController::class,'destroy']);
