55d5b894409ca9f5fea52f7c5e51fa2bba16adf3
Menar Selamet <menarselamet007@gmail.com>
2024-08-18 19:57:59 +0300

    Breaking the form into smaller componenets

--- newjobs/routes/web.php
+++ newjobs/routes/web.php
@@ -8,14 +8,3 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact','contact');
 Route::resource('jobs', JobController::class);
-
-
-// Route::controller(JobController::class)->group(function(){
-// Route::get('/jobs', 'index'); 
-// Route::get('/jobs/create','create');
-// Route::post('/jobs','store');
-// Route::get('/jobs/{job}','show');
-// Route::get('/jobs/{job}/edit','edit');
-// Route::patch('/jobs/{job}','update');
-// Route::delete('/jobs/{job}','destroy');
-// });
