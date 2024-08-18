c4eb1cdecfe79c6b0cddc5486111cf7d457229aa
Menar Selamet <menarselamet007@gmail.com>
2024-08-18 15:38:17 +0300

    Groupping Jobcontroller routes

--- newjobs/routes/web.php
+++ newjobs/routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 
 Route::view('/', 'home');
 Route::view('/contact','contact');
-Route::get('/jobs', [JobController::class, 'index']); 
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::post('/jobs', [JobController::class,'store']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::get('/jobs/{job}/edit', [JobController::class,'edit']);
-Route::patch('/jobs/{job}',[JobController::class,'update']);
-Route::delete('/jobs/{job}', [JobController::class,'destroy']);
+
+Route::controller(JobController::class)->group(function(){
+Route::get('/jobs', 'index'); 
+Route::get('/jobs/create','create');
+Route::post('/jobs','store');
+Route::get('/jobs/{job}','show');
+Route::get('/jobs/{job}/edit','edit');
+Route::patch('/jobs/{job}','update');
+Route::delete('/jobs/{job}','destroy');
+});
