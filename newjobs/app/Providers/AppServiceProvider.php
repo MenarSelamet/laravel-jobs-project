fabc07252dad8f62460d60019f718566563a1718
Menar Selamet <menarselamet007@gmail.com>
2024-08-20 06:51:23 +0300

    Creating a Job Policy to conditionally show and allow edit

--- newjobs/app/Providers/AppServiceProvider.php
+++ newjobs/app/Providers/AppServiceProvider.php
@@ -25,9 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Model::preventLazyLoading();
-        Gate::define('edit-job', function (User $user, Job $job) {
-            return $job->employer->user->is($user);
-        });
+        // Model::preventLazyLoading();
+        // Gate::define('edit-job', function (User $user, Job $job) {
+        //     return $job->employer->user->is($user);
+        // });
     }
 }
--- newjobs/resources/views/jobs/show.blade.php
+++ newjobs/resources/views/jobs/show.blade.php
@@ -5,7 +5,7 @@
     <h2><strong>{{ $job->title}}</strong></h2>
     <p>This job pays {{ $job->salary }} per year</p>
     <p class="mt-10">
-        @can('edit-job', $job)
+        @can('edit', $job)
         <x-button href="/jobs/{{$job->id}}/edit">
             Edit Job
         </x-button>
