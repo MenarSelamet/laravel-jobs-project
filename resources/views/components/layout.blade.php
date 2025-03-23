ced8f46692fdb0c2c21eb36515e32460bb0c535c
Menar Selamet <menarselamet007@gmail.com>
2025-03-23 06:26:55 +0300

    Minor design changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,7 +17,7 @@
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
                         <div class="flex-shrink-0">
-                            <img class="h-8 w-8" src="https://laracasts.com/images/logo/logo-triangle.svg"
+                            <img class="h-8 w-8" src="logo-M.jpg"
                                 alt="Your Company">
                         </div>
                         <div class="hidden md:block">
@@ -77,16 +77,16 @@
                     <a href="/contact"
                         class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
                 </div>
-                <div class="border-t border-gray-700 pb-3 pt-4">
-                    <div class="flex items-center px-5">
-                        <div class="flex-shrink-0">
-                            <img class="h-10 w-10 rounded-full" src="https://laracasts.com/images/lary-ai-face.svg"
+                    <div class="border-t border-gray-700 pb-3 pt-4">
+                        <div class="flex items-center px-5">
+                            <div class="flex-shrink-0">
+                            <img class="h-10 w-10 rounded-full" src="logo-M.jpg"
                                 alt="">
-                        </div>
-                        <div class="ml-3">
-                            <div class="text-base font-medium leading-none text-white">Lary Robot</div>
-                            <div class="text-sm font-medium leading-none text-gray-400">jeffrey@laracasts.com</div>
-                        </div>
+                            </div>
+                            <div class="ml-3">
+                                <div class="text-base font-medium leading-none text-white">Angry Llama</div>
+                                <div class="text-sm font-medium leading-none text-gray-400">lamawithdrama@lama.com</div>
+                            </div>
                         <button type="button"
                             class="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span class="absolute -inset-1.5"></span>
@@ -96,9 +96,9 @@
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                             </svg>
-                        </button>
+                                </button>
+                        </div>
                     </div>
-                </div>
             </div>
         </nav>
 
